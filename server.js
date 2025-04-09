12cb1b9a8dfefe70a2d92068ec747999664c6fdf
bhavik <bhavik.bhagwani0001@stud.hkr.se>
2025-04-09 21:41:18 +0200

    added views, table with update and delete functionality

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
-import { client, connectToMongoDB} from "./src/db/connect.js";
+
+import { connectToMongoDB} from "./src/db/connect.js";
 import { router as dishesRouter } from './src/routes/dishesRoute.js';
 import { globalErrorHandler, notFoundErrorHandler } from './src/middleware/errorHandler.js';
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.use(express.json());
 
